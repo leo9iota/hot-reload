3457890d9da3930cd7d4f106dc9f6a5de8124a5c
leo9iota <leo9iota@gmail.com>
2025-09-05 23:07:47 +0200

    feat(multiplayer): implement peer readiness notification

--- main.gd
+++ main.gd
@@ -2,4 +2,8 @@ extends Node
 
 
 func _ready() -> void:
-	print("Hello, world!")
+	peer_ready.rpc_id(1)
+	
+@rpc("any_peer", "call_local", "reliable")
+func peer_ready():
+	print("Peer %s ready" % multiplayer.get_remote_sender_id() )
