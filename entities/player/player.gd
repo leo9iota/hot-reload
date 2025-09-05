c1b96322c2c512a25ab9b82b6768db38daa58caf
leo9iota <leo9iota@gmail.com>
2025-09-05 20:37:18 +0200

    chore(general): apply formatting

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,8 +2,6 @@ extends CharacterBody2D
 
 
 func _process(delta: float) -> void:
-	var movement_vector = Input.get_vector(
-		"move_left", "move_right", "move_up", "move_down"
-	)
+	var movement_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = movement_vector * 100
 	move_and_slide()
