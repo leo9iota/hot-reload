a2cd3f779056257238e41d346fe99e8b199133ed
leo9iota <leo9iota@gmail.com>
2025-09-05 20:50:27 +0200

    chore(ui): rename button

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -1,17 +1,17 @@
 extends Control
 
 @onready var host_button: Button = $HBoxContainer/HostButton
-@onready var play_button: Button = $HBoxContainer/PlayButton
+@onready var join_button: Button = $HBoxContainer/JoinButton
 
 
 func _ready() -> void:
 	host_button.pressed.connect(_on_host_pressed)
-	play_button.pressed.connect(_on_play_pressed)
+	join_button.pressed.connect(_on_join_pressed)
 
 
 func _on_host_pressed() -> void:
 	print("Host button pressed")
 
 
-func _on_play_pressed() -> void:
+func _on_join_pressed() -> void:
 	print("Play button pressed")
