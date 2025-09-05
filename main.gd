f06ed2fcde7970ff4df3ef456719723faf03f877
leo9iota <leo9iota@gmail.com>
2025-09-05 20:00:19 +0200

    chore(general): apply formatting

--- main.gd
+++ main.gd
@@ -1,4 +1,5 @@
 extends Node
 
+
 func _ready() -> void:
 	print("Hello, world!")
--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -3,6 +3,7 @@ extends Control
 @onready var host_button: Button = $HBoxContainer/HostButton
 @onready var play_button: Button = $HBoxContainer/PlayButton
 
+
 func _ready() -> void:
 	host_button.pressed.connect(_on_host_pressed)
 	play_button.pressed.connect(_on_play_pressed)
