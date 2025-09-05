601fe59f0bc9e95cb26322e2765ec01f7ddb822b
leo9iota <leo9iota@gmail.com>
2025-09-05 19:29:36 +0200

    feat(menu): add main menu UI with button functionality

---
+++ ui/main_menu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var host_button: Button = $HBoxContainer/HostButton
+@onready var play_button: Button = $HBoxContainer/PlayButton
+
+func _ready() -> void:
+	host_button.pressed.connect(_on_host_pressed)
+	play_button.pressed.connect(_on_play_pressed)
+
+
+func _on_host_pressed() -> void:
+	print("Host button pressed")
+
+
+func _on_play_pressed() -> void:
+	print("Play button pressed")
