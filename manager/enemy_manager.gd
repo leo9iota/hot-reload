8200b429da0154de451103d9badabc6a94234e8c
leo9iota <leo9iota@gmail.com>
2025-09-07 20:41:27 +0200

    feat(enemy): implement enemy manager for spawning enemies

---
+++ manager/enemy_manager.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var enemy_scene: PackedScene
+@export var enemy_spawn_root: Node
+
+@onready var spawn_interval_timer: Timer = $SpawnIntervalTimer
+
+
+func _ready():
+	spawn_interval_timer.timeout.connect(_on_spawn_interval_timer_timeout)
+	
+func spawn_enemy():
+	var enemy = enemy_scene.instantiate() as Node2D
+	enemy.global_position = Vector2.ONE * 200
+	enemy_spawn_root.add_child(enemy, true)
+	
+func _on_spawn_interval_timer_timeout():
+	if is_multiplayer_authority():
+		spawn_enemy()
+ 
