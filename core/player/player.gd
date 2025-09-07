02bef3fa0982d19ff5254f122ffcd8c5694cd919
leo9iota <leo9iota@gmail.com>
2025-09-07 17:15:35 +0200

    feat(player): refactor bullet creation logic and ensure fire rate control

--- core/player/player.gd
+++ core/player/player.gd
@@ -2,6 +2,7 @@ class_name Player extends CharacterBody2D
 
 @onready var player_input_synchronizer_component: PlayerInputSynchronizerComponent = $PlayerInputSynchronizerComponent
 @onready var weapon_root: Node2D = $WeaponRoot
+@onready var fire_rate_timer: Timer = $FireRateTimer
 
 var bullet_scene: PackedScene = preload("uid://clscgesvupype")
 
@@ -20,11 +21,15 @@ func _process(delta: float) -> void:
 		velocity = player_input_synchronizer_component.movement_vector * 100
 		move_and_slide()
 		if player_input_synchronizer_component.is_attack_pressed:
-			create_bullet()
+			try_create_bullet()
 
 
-func create_bullet():
+func try_create_bullet():
+	if !fire_rate_timer.is_stopped():
+		return
+
 	var bullet = bullet_scene.instantiate() as Bullet
 	bullet.global_position = weapon_root.global_position
 	bullet.start(player_input_synchronizer_component.aim_vector)
 	get_parent().add_child(bullet, true)
+	fire_rate_timer.start()
