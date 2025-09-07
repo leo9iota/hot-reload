540da919782cb06bec5d322aba39beaf8a98ea5e
leo9iota <leo9iota@gmail.com>
2025-09-07 21:04:01 +0200

    refactor(main): remove unused enemy scene preload and instantiation code

--- main.gd
+++ main.gd
@@ -1,7 +1,6 @@
 extends Node
 
 var player_scene: PackedScene = preload("uid://bq3ewxhqr2hif")
-var enemy_scene: PackedScene = preload("uid://c3nm61v3vr63j")
 
 @onready var multiplayer_spawner: MultiplayerSpawner = $MultiplayerSpawner
 @onready var player_spawn_position: Marker2D = $PlayerSpawnPosition
@@ -13,14 +12,12 @@ func _ready():
 		player.name = str(data.peer_id)
 		player.input_multiplayer_authority = data.peer_id
 		player.global_position = player_spawn_position.global_position
+		
 		return player
 
 	peer_ready.rpc_id(1)
 
-	if is_multiplayer_authority():
-		var enemy = enemy_scene.instantiate() as Node2D
-		enemy.global_position = Vector2.ONE * 200
-		add_child(enemy)
+
 
 
 @rpc("any_peer", "call_local", "reliable")
