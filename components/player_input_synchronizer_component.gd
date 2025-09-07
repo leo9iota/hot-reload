5ce9c87f1659bf060732cfa724c6710f48f8c744
leo9iota <leo9iota@gmail.com>
2025-09-07 15:37:31 +0200

    refactor(player): rename `get_input` to `receive_player_input` for clarity

--- components/player_input_synchronizer_component.gd
+++ components/player_input_synchronizer_component.gd
@@ -8,8 +8,9 @@ var aim_vector: Vector2 = Vector2.RIGHT
 
 func _process(_delta: float) -> void:
 	if is_multiplayer_authority():
-		get_input()
+		receive_player_input()
 
 
-func get_input():
+func receive_player_input():
 	movement_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	aim_vector = aim_root.global_position.direction_to(aim_root.get_global_mouse_position())
