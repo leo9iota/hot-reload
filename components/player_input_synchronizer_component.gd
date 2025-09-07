2517add4ccc3ff8ffdd77b8c005ac8b4c68d3d53
leo9iota <leo9iota@gmail.com>
2025-09-07 16:31:44 +0200

    feat(player): add attack input handling to `PlayerInputSynchronizerComponent`

--- components/player_input_synchronizer_component.gd
+++ components/player_input_synchronizer_component.gd
@@ -4,6 +4,7 @@ class_name PlayerInputSynchronizerComponent extends MultiplayerSynchronizer
 
 var movement_vector: Vector2 = Vector2.ZERO
 var aim_vector: Vector2 = Vector2.RIGHT
+var is_attack_pressed: bool
 
 
 func _process(_delta: float) -> void:
@@ -14,3 +15,4 @@ func _process(_delta: float) -> void:
 func receive_player_input():
 	movement_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	aim_vector = aim_root.global_position.direction_to(aim_root.get_global_mouse_position())
+	is_attack_pressed = Input.is_action_pressed("attack")
