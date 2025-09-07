7ae5ecfdf7b12de52824329069e5e4fcbcd8f281
leo9iota <leo9iota@gmail.com>
2025-09-07 17:15:01 +0200

    feat(bullet): initialize life timer and connect timeout signal

--- core/bullet/bullet.gd
+++ core/bullet/bullet.gd
@@ -2,7 +2,13 @@ class_name Bullet extends Node2D
 
 const SPEED: int = 600
 
+@onready var life_timer: Timer = $LifeTimer
+
 var direction: Vector2
+
+
+func _ready() -> void:
+	life_timer.timeout.connect(_on_life_timer_timeout)
 
 
 func _process(delta: float) -> void:
@@ -12,3 +18,8 @@ func _process(delta: float) -> void:
 func start(direction: Vector2):
 	self.direction = direction
 	rotation = direction.angle()
+
+
+func _on_life_timer_timeout():
+	if is_multiplayer_authority():
+		queue_free()
