946089aa3a89a3ce9da18be13222c2f79c3e2b9e
leo9iota <leo9iota@gmail.com>
2025-09-08 20:55:37 +0200

    feat(health): add `HealthComponent` to manage health and death signals

---
+++ components/health/health_component.gd
@@ -0,0 +1,20 @@
+extends Node
+
+signal died
+
+# In the 'health_component' and 'player_input_synchronizer_component',
+# we use the composition pattern
+@export var max_health: int = 1
+
+var current_health: int = 0
+
+
+func _ready() -> void:
+	current_health = max_health
+
+
+func damage(amount: int):
+	current_health = clamp(current_health - amount, 0, max_health)
+
+	if current_health == 0:
+		died.emit()
