c422fe3eee357945742969d5343cf2c32cb9a503
leo9iota <leo9iota@gmail.com>
2025-09-10 03:06:17 +0200

    feat(bullet): connect hitbox signal to handle collisions with hurtboxes

--- core/bullet/bullet.gd
+++ core/bullet/bullet.gd
@@ -3,11 +3,13 @@ class_name Bullet extends Node2D
 const SPEED: int = 600
 
 @onready var life_timer: Timer = $LifeTimer
+@onready var hitbox_component: HitboxComponent = $HitboxComponent
 
 var direction: Vector2
 
 
 func _ready() -> void:
+	hitbox_component.hit_hurtbox.connect(_on_hit_hurtbox)
 	life_timer.timeout.connect(_on_life_timer_timeout)
 
 
@@ -27,3 +29,10 @@ func register_collision():
 func _on_life_timer_timeout():
 	if is_multiplayer_authority():
 		queue_free()
+
+
+# We don't use the '_hurtbox_component', but we do need to accept it,
+# because the signal handler's signature need to match the argument list
+# that is emitted with the signal.
+func _on_hit_hurtbox(_hurtbox_component: HurtboxComponent):
+	register_collision()
