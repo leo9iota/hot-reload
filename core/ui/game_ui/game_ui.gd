37ce3db3d4f4add47f32409232cf3a4644c6b6fd
leo9iota <leo9iota@gmail.com>
2025-09-12 02:49:13 +0200

    feat(ui): update TimerLabel reference and round time display formatting

--- core/ui/game_ui/game_ui.gd
+++ core/ui/game_ui/game_ui.gd
@@ -1,8 +1,9 @@
 extends CanvasLayer
 
 @export var enemy_manager: EnemeyManager
-@onready var timer_label: Label = %TimerLabel
+@onready var timer_label: Label = %RoundTimerLabel
+@onready var round_count_label: Label = %RoundCountLabel
 
 
 func _process(_delta: float) -> void:
-	timer_label.text = str(enemy_manager.get_round_time_remaining())
+	timer_label.text = str(ceili(enemy_manager.get_round_time_remaining()))
