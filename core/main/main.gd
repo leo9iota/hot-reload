5178a756aca5935470a5a02425bc43048fac5e68
leo9iota <leo9iota@gmail.com>
2025-09-14 00:58:01 +0200

    feat(multiplayer): implement dead peer respawn functionality

--- core/main/main.gd
+++ core/main/main.gd
@@ -6,6 +6,8 @@ var player_scene: PackedScene = preload("uid://bq3ewxhqr2hif")
 @onready var player_spawn_position: Marker2D = $PlayerSpawnPosition
 @onready var enemy_manager: EnemyManager = $EnemyManager
 
+var dead_peers: Array[int] = []
+
 
 func _ready():
 	multiplayer_spawner.spawn_function = func(data):
@@ -14,9 +16,13 @@ func _ready():
 		player.input_multiplayer_authority = (data.peer_id)
 		player.global_position = (player_spawn_position.global_position)
 
+		if is_multiplayer_authority():
+			player.died.connect(_on_player_died.bind(data.peer_id))
+
 		return player
 
 	peer_ready.rpc_id(1)
+	enemy_manager.round_completed.connect(_on_round_completed)
 
 
 @rpc("any_peer", "call_local", "reliable")
@@ -24,3 +30,18 @@ func peer_ready():
 	var sender_id = multiplayer.get_remote_sender_id()
 	multiplayer_spawner.spawn({"peer_id": sender_id})
 	enemy_manager.sync_server(sender_id)
+
+
+func respawn_dead_peers():
+	for peer_id in dead_peers:
+		multiplayer_spawner.spawn({"peer_id": peer_id})
+
+	dead_peers.clear()
+
+
+func _on_player_died(peer_id: int):
+	dead_peers.append(peer_id)
+
+
+func _on_round_completed():
+	respawn_dead_peers()
