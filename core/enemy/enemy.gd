05385172aba45d14d1dd8634b13c329c6ae8d35a
leo9iota <leo9iota@gmail.com>
2025-09-13 18:48:32 +0200

    refactor(enemy): update enemy spawn logic
    - Improve enemy spawn logic
    - Add tween animation on spawn

--- core/enemy/enemy.gd
+++ core/enemy/enemy.gd
@@ -2,12 +2,18 @@ extends CharacterBody2D
 
 @onready var target_acquisition_timer: Timer = $TargetAcquisitionTimer
 @onready var health_component: HealthComponent = $HealthComponent
+@onready var visuals: Node2D = $Visuals
 
 var target_position: Vector2
+var is_spawning: bool
 
 
 func _ready() -> void:
-	target_acquisition_timer.timeout.connect(_on_target_acquisition_timer_timeout)
+	target_acquisition_timer.timeout.connect(
+		_on_target_acquisition_timer_timeout
+	)
+
+	play_spawn_animation()
 
 	if is_multiplayer_authority():
 		health_component.died.connect(_on_died)
@@ -16,9 +22,33 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	# Only set velocity and move enemy if we're on the server-side
-	if is_multiplayer_authority():
+	if is_multiplayer_authority() && not is_spawning:
 		velocity = global_position.direction_to(target_position) * 50
 		move_and_slide()
+		
+	if not is_spawning:
+		flip_enemy_character()
+
+
+# Flips the enemy character asset into specific direction
+func flip_enemy_character():
+	visuals.scale = (
+		Vector2.ONE if target_position.x > global_position.x else Vector2(-1, 1)
+	)
+
+
+func play_spawn_animation():
+	is_spawning = true
+	var tween := create_tween()
+	(
+		tween
+		. tween_property(visuals, "scale", Vector2.ONE, 0.4)
+		. from(Vector2.ZERO)
+		. set_ease(Tween.EASE_OUT)
+		. set_trans(Tween.TRANS_BACK)
+	)
+	await tween.finished
+	is_spawning = false
 
 
 func acquire_target():
@@ -31,10 +61,16 @@ func acquire_target():
 	for player in players:
 		if nearest_player == null:
 			nearest_player = player
-			nearest_squared_distance = nearest_player.global_position.distance_squared_to(global_position)
+			nearest_squared_distance = (
+				nearest_player
+				. global_position
+				. distance_squared_to(global_position)
+			)
 			continue
 
-		var player_squared_distance: float = player.global_position.distance_squared_to(global_position)
+		var player_squared_distance: float = (
+			player.global_position.distance_squared_to(global_position)
+		)
 
 		if player_squared_distance < nearest_squared_distance:
 			nearest_squared_distance = player_squared_distance
