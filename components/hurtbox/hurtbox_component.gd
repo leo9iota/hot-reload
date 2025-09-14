2f92864ddcf99db92b598731fb1e6b052a06478c
leo9iota <leo9iota@gmail.com>
2025-09-14 19:30:55 +0200

    feat(hurtbox): emit signal when hit by hitbox

--- components/hurtbox/hurtbox_component.gd
+++ components/hurtbox/hurtbox_component.gd
@@ -1,5 +1,7 @@
 class_name HurtboxComponent extends Area2D
 
+signal hit_by_hitbox
+
 @export var health_component: HealthComponent
 
 
@@ -10,6 +12,7 @@ func _ready() -> void:
 func _handle_hit(hitbox_component: HitboxComponent):
 	hitbox_component.register_hurtbox_hit(self)
 	health_component.damage(hitbox_component.damage)
+	hit_by_hitbox.emit()
 
 
 func _on_area_entered(other_area: Area2D):
