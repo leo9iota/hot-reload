07cd3f92d326de600577c78aaf74b4fef785ffea
leo9iota <leo9iota@gmail.com>
2025-09-14 19:31:09 +0200

    feat(hurtbox): connect hurtbox to hitbox signal and spawn impact particles on hit

--- core/enemy/enemy.gd
+++ core/enemy/enemy.gd
@@ -7,6 +7,9 @@ extends CharacterBody2D
 @onready var charge_attack_timer: Timer = $ChargeAttackTimer
 @onready var hitbox_collision_shape: CollisionShape2D = %HitboxCollisionShape
 @onready var alert_sprite: Sprite2D = $AlertSprite
+@onready var hurtbox_component: HurtboxComponent = $HurtboxComponent
+
+var impact_particles_scene: PackedScene = preload("uid://bsrtogn8safuu")
 
 const DASH_SPEED: int = 400
 const LERP_SMOOTHING: int = -15
@@ -47,6 +50,7 @@ func _ready() -> void:
 	if is_multiplayer_authority():
 		health_component.died.connect(_on_died)
 		state_machine.set_initial_state(state_spawn)
+		hurtbox_component.hit_by_hitbox.connect(_on_hit_by_hitbox)
 
 
 func _process(_delta: float) -> void:
@@ -202,6 +206,17 @@ func acquire_target():
 		target_position = (nearest_player.global_position)
 
 
+@rpc("authority", "call_local")
+func spawn_hit_particles():
+	var hit_particles: Node2D = impact_particles_scene.instantiate()
+	hit_particles.global_position = hurtbox_component.global_position
+	get_parent().add_child(hit_particles)
+
+
 func _on_died():
 	GameEvents.emit_enemy_died()
 	queue_free()
+
+
+func _on_hit_by_hitbox():
+	spawn_hit_particles.rpc()
