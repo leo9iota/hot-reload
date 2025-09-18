0eb8ac30d0e3d47533eb213c88be8cae25c547ef
leo9iota <leo9iota@gmail.com>
2025-09-18 18:27:42 +0200

    feat(multiplayer): add back button functionality to return to main menu

--- core/ui/multiplayer_menu/multiplayer_menu.gd
+++ core/ui/multiplayer_menu/multiplayer_menu.gd
@@ -4,15 +4,21 @@ const PORT: int = 3000
 
 @onready var host_button: Button = %HostButton
 @onready var join_button: Button = %JoinButton
+@onready var back_button: Button = %BackButton
 
 var main_scene: PackedScene = preload("uid://cpcymwvh3h4ny")
+var main_menu_scene: PackedScene = preload("uid://cv72gepsh2eqd")
 
 
 func _ready() -> void:
+	back_button.pressed.connect(_on_back_button_pressed)
 	host_button.pressed.connect(_on_host_button_pressed)
 	join_button.pressed.connect(_on_join_button_pressed)
 	multiplayer.connected_to_server.connect(_on_connected_to_server)
 
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_packed(main_menu_scene)
 
 func _on_host_button_pressed() -> void:
 	var server_peer := ENetMultiplayerPeer.new()
