22604786722ae504ceeec7f16d442ecf3b26bf10
leo9iota <leo9iota@gmail.com>
2025-09-19 01:37:35 +0200

    refactor(audio): simplify button registration in `UIAudioManager`

--- core/autoload/ui_audio_manager.gd
+++ core/autoload/ui_audio_manager.gd
@@ -2,17 +2,13 @@ extends Node
 
 @onready var button_stream_player: AudioStreamPlayer = $ButtonStreamPlayer
 
-static var instance: UIAudioManager
 
 
-func _ready() -> void:
-	instance = self
-
-
-static func register_buttons(buttons: Array):
+func register_buttons(buttons: Array):
 	for button in buttons:
-		button.pressed.connect(instance._on_button_pressed)
+		if not button.pressed.is_connected(_on_button_pressed):
+			button.pressed.connect(_on_button_pressed)
 
 
 func _on_button_pressed():
-	instance.button_stream_player.play()
+	button_stream_player.play()
