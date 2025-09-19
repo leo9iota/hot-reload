556221de93d107ffa8c947ffe30de0886cdf959b
leo9iota <leo9iota@gmail.com>
2025-09-20 00:33:34 +0200

    feat(pause_menu): implement RPC for pause and unpause

--- core/ui/pause_menu/pause_menu.gd
+++ core/ui/pause_menu/pause_menu.gd
@@ -1,12 +1,42 @@
 extends CanvasLayer
 
+var current_paused_peer: int = -1
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
-		toggle_pause()
+		if get_tree().paused:
+			request_unpause.rpc_id(MultiplayerPeer.TARGET_PEER_SERVER)
+		else:
+			request_pause.rpc_id(MultiplayerPeer.TARGET_PEER_SERVER)
 		get_viewport().set_input_as_handled()
-		
-		
-func toggle_pause():
-	visible = not visible
-	get_tree().paused = visible
+
+
+@rpc("any_peer", "call_local", "reliable")
+func request_pause():
+	if current_paused_peer > -1:
+		return
+
+	current_paused_peer = multiplayer.get_remote_sender_id()
+	pause.rpc()
+
+
+@rpc("any_peer", "call_local", "reliable")
+func request_unpause():
+	if current_paused_peer != multiplayer.get_remote_sender_id():
+		return
+
+	current_paused_peer = -1
+	unpause.rpc()
+
+
+@rpc("any_peer", "call_local", "reliable")
+func pause():
+	get_tree().paused = true
+	visible = true
+
+
+@rpc("any_peer", "call_local", "reliable")
+func unpause():
+	get_tree().paused = false
+	visible = false
