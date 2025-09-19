002591e617e7cc5faf12ee37fac9f01536e317bb
leo9iota <leo9iota@gmail.com>
2025-09-19 22:40:35 +0200

    feat(ui): implement player connection and health bar updates

--- core/ui/game_ui/game_ui.gd
+++ core/ui/game_ui/game_ui.gd
@@ -1,8 +1,10 @@
-extends CanvasLayer
+class_name GameUI extends CanvasLayer
 
 @export var enemy_manager: EnemyManager
 @onready var round_timer_label: Label = %RoundTimerLabel
 @onready var round_count_label: Label = %RoundCountLabel
+@onready var health_bar: ProgressBar = %HealthBar
+@onready var username_label: Label = %UsernameLabel
 
 
 func _ready() -> void:
@@ -15,5 +17,24 @@ func _process(_delta: float) -> void:
 	)
 
 
+func connect_player(player: Player):
+	(
+		(func():
+			username_label.text = player.username
+			player.health_component.health_changed.connect(_on_health_changed)
+			_on_health_changed(
+				player.health_component.current_health,
+				player.health_component.max_health
+			))
+		. call_deferred()
+	)
+
+
 func _on_round_began(round_count: int):
 	round_count_label.text = ("Round %s" % round_count)
+
+
+func _on_health_changed(current_health: int, max_health: int):
+	health_bar.value = (
+		(float(current_health) / float(max_health)) if max_health != 0 else 0.0
+	)
