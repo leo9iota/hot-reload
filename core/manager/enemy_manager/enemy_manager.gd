4fab617b7e4684db0560ce12bbc13928cd209b58
leo9iota <leo9iota@gmail.com>
2025-09-20 23:21:43 +0200

    feat(enemy-manager): emit `round_completed` signal before starting a new round

--- core/manager/enemy_manager/enemy_manager.gd
+++ core/manager/enemy_manager/enemy_manager.gd
@@ -99,12 +99,10 @@ func check_round_completed():
 		return
 
 	if spawned_enemies == 0:
-		round_completed.emit()
-
 		if round_count == MAX_ROUNDS:
 			complete_game()
 		else:
-			begin_round()
+			round_completed.emit()
 
 
 func complete_game():
