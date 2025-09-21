5a677510a9dd62897b30ebb66b8a76bafa72f21e
leo9iota <leo9iota@gmail.com>
2025-09-21 17:31:51 +0200

    feat(hitbox): move source_peer_id declaration to the correct position

--- components/hitbox/hitbox_component.gd
+++ components/hitbox/hitbox_component.gd
@@ -3,7 +3,6 @@ class_name HitboxComponent extends Area2D
 signal hit_hurtbox(hurtbox_component: HurtboxComponent)
 
 var damage: int = 1
-
-
+var source_peer_id: int
 func register_hurtbox_hit(hurtbox_component: HurtboxComponent):
 	hit_hurtbox.emit(hurtbox_component)
